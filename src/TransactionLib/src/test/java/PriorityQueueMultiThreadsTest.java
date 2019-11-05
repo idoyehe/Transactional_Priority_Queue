d02dc5896dc5a44ef8e88035865f725a655308da
IdoYe <idoye@campus.technion.ac.il>
2019-11-05 17:08:27 +0200

    handling node with same priority sorting by enqueue time

--- src/TransactionLib/src/test/java/PriorityQueueMultiThreadsTest.java
+++ src/TransactionLib/src/test/java/PriorityQueueMultiThreadsTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import static junit.framework.TestCase.*;
 
 import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 import java.util.stream.IntStream;
 
@@ -127,34 +126,30 @@ public class PriorityQueueMultiThreadsTest {
     public void testPriorityQueueMultiThreadTransaction() throws InterruptedException {
         PriorityQueue pQueue = new PriorityQueue();
         pQueue.setSingleton(false);
-        CountDownLatch latch = new CountDownLatch(1);
         CyclicBarrier barrier = new CyclicBarrier(this.numberOfThreads);
 
         Thread[] threadsARR = new Thread[this.numberOfThreads];
         for (int i = 0; i < this.numberOfThreads; i++) {
-            threadsARR[i] = new Thread(new RunTransaction("T" + i, latch, barrier, pQueue, i * numberOfThreads, numberOfThreads));
+            threadsARR[i] = new Thread(new RunTransaction("T" + i, barrier, pQueue, i * numberOfThreads, numberOfThreads));
             threadsARR[i].start();
         }
-        latch.countDown();
         for (int i = 0; i < this.numberOfThreads; i++) {
             threadsARR[i].join();
         }
     }
 
     class RunTransaction extends RunSingletone {
-        private CountDownLatch latch;
 
 
-        RunTransaction(String name, CountDownLatch latch, CyclicBarrier barrier, PriorityQueue pq, int priorityRef, int numberOfThread) {
+        RunTransaction(String name, CyclicBarrier barrier, PriorityQueue pq, int priorityRef, int numberOfThread) {
             super(name, barrier, pq, priorityRef, numberOfThread);
-            this.latch = latch;
         }
 
         @Override
         public void run() {
             try {
-                latch.await();
-            } catch (InterruptedException exp) {
+                this.barrier.await();
+            } catch (BrokenBarrierException | InterruptedException exp) {
                 System.out.println(threadName + ": InterruptedException");
             }
             PQNode globalNodesArr[] = new PQNode[this.numberOfThread];
