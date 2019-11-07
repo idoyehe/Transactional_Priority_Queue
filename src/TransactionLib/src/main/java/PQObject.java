2988e4d5e7f28b640b45105c3a4e9c3eb23524d2
IdoYe <idoye@campus.technion.ac.il>
2019-11-07 21:50:00 +0200

    fix all tests, this version ready for tests

--- src/TransactionLib/src/main/java/PQObject.java
+++ src/TransactionLib/src/main/java/PQObject.java
@@ -1,38 +1,28 @@
 package TransactionLib.src.main.java;
 
 public class PQObject implements Comparable<PQObject> {
-    private long ref;
-    private long time;
-    private Comparable priority;
-    private Object value;
-    private boolean isIgnored = false;
+    private int index = -1;// index of the node in heap
+    private Comparable priority = null;
+    private Object value = null;
+    private boolean _isIgnored = false;
 
     public PQObject(Comparable priority, Object value) {
         this.priority = priority;
         this.value = value;
-        this.time = 0;
     }
 
-    public PQObject(Comparable priority, Object value, long time) {
-        this(priority, value);
-        this.time = time;
-    }
-
-    public PQObject(PQObject node) {
-        assert node != null;
-        this.priority = node.getPriority();
-        this.value = node.getValue();
-        this.time = node.getTime();
+    public PQObject(PQObject pqObject) {
+        this(pqObject.getPriority(), pqObject.getValue());
     }
 
     //getters
 
-    public final Comparable getPriority() {
-        return this.priority;
+    public int getIndex() {
+        return this.index;
     }
 
-    public final long getTime() {
-        return this.time;
+    public final Comparable getPriority() {
+        return this.priority;
     }
 
     public Object getValue() {
@@ -40,43 +30,27 @@ public class PQObject implements Comparable<PQObject> {
     }
 
     public boolean getIsIgnored() {
-        return this.isIgnored;
+        return this._isIgnored;
     }
 
-    public long getRef() {
-        return this.ref;
-    }
 
     //setters
-
-    PQObject setTime(final long newTime) {
-        this.time = newTime;
-        return this;
-    }
 
     void setPriority(final Comparable newPriority) {
         this.priority = newPriority;
     }
 
-    void setRef(long ref) {
-        this.ref = ref;
-    }
-
     public void setIgnored() {
-        assert !this.isIgnored;
-        this.isIgnored = true;
+        assert !this._isIgnored;
+        this._isIgnored = true;
     }
 
     @Override
-    public int compareTo(PQObject pqNode) {
-        int ret = this.getPriority().compareTo(pqNode.getPriority());
-        if (ret != 0) {
-            return ret;
-        }
-        return (int) Math.signum(pqNode.getTime() - this.getTime());
+    public int compareTo(PQObject PQObject) {
+        return this.getPriority().compareTo(PQObject.getPriority());
     }
 
-    public boolean isContentEqual(PQObject pqNode) {
-        return this.getPriority().compareTo(pqNode.getPriority()) == 0 && this.value.equals(pqNode.value);
+    public boolean isContentEqual(PQObject PQObject) {
+        return this.getPriority().compareTo(PQObject.getPriority()) == 0 && this.value.equals(PQObject.value);
     }
 }
