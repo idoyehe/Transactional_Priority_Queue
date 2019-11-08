367722957e11b1ce363d56a41e512ad841f0a13f
IdoYe <idoye@campus.technion.ac.il>
2019-11-08 16:20:43 +0200

    adding functions documentation

--- src/TransactionLib/src/main/java/PQObject.java
+++ src/TransactionLib/src/main/java/PQObject.java
@@ -6,29 +6,52 @@ public class PQObject implements Comparable<PQObject> {
     private Object value = null;
     private boolean _isIgnored = false;
 
+    /**
+     * New PQObject Constructor
+     *
+     * @param priority the priority of the new node
+     * @param value    the value of new node
+     */
     public PQObject(Comparable priority, Object value) {
         this.priority = priority;
         this.value = value;
     }
 
+    /**
+     * PQObject Copy Constructor
+     *
+     * @param pqObject this node priority and value are copied to create new node
+     */
     public PQObject(PQObject pqObject) {
         this(pqObject.getPriority(), pqObject.getValue());
     }
 
     //getters
 
+    /**
+     * @return the index of the node, can be null
+     */
     public int getIndex() {
         return this.index;
     }
 
+    /**
+     * @return the priority of the node, can be null
+     */
     public final Comparable getPriority() {
         return this.priority;
     }
 
+    /**
+     * @return the value of the node, can be null
+     */
     public Object getValue() {
         return this.value;
     }
 
+    /**
+     * @return the ignore status of the node
+     */
     public boolean getIsIgnored() {
         return this._isIgnored;
     }
@@ -36,45 +59,89 @@ public class PQObject implements Comparable<PQObject> {
 
     //setters
 
+    /**
+     * setter of PQObject priority
+     *
+     * @param newPriority the new priority to be
+     */
     void setPriority(final Comparable newPriority) {
         this.priority = newPriority;
     }
 
+    /**
+     * mark the node as ignored
+     */
     public void setIgnored() {
         assert !this._isIgnored;
         this._isIgnored = true;
     }
 
+    /**
+     * setter of PQNode index
+     *
+     * @param newIndex the new index to be
+     */
     void setIndex(int value) {
         this.index = value;
     }
 
-
+    /**
+     * Compering 2 PQObject priority
+     *
+     * @param pqObject node to compere to
+     * @return -1 if this.priority < pqNode.priority;
+     * 0 if this.priority == pqNode.priority;
+     * 1 if this.priority > pqNode.priority
+     */
     @Override
-    public int compareTo(PQObject PQObject) {
-        return this.getPriority().compareTo(PQObject.getPriority());
+    public int compareTo(PQObject pqObject) {
+        return this.getPriority().compareTo(pqObject.getPriority());
     }
 
+    /**
+     * Compering this.priority to given priority
+     *
+     * @param priority to compere to
+     * @return -1 if this.priority < priority;
+     * 0 if this.priority == priority;
+     * 1 if this.priority > priority
+     */
     public int compareTo(Comparable priority) {
         return this.getPriority().compareTo(priority);
     }
 
-    public boolean isContentEqual(PQObject PQObject) {
-        return this.getPriority().compareTo(PQObject.getPriority()) == 0 && this.value.equals(PQObject.value);
+    /**
+     * Check if 2 PQNodes content is equal
+     *
+     * @param pqObject node to compere to
+     * @return true if this.priority == pqNode.priority && this.value == this.value.equals(pqNode.value)
+     * otherwise false
+     */
+    public boolean isContentEqual(PQObject pqObject) {
+        return this.getPriority().compareTo(pqObject.getPriority()) == 0 && this.value.equals(pqObject.value);
     }
 
-    // to get index of parent of node at index i
-    static int parent(int i) {
-        return (i - 1) / 2;
+    /**
+     * @param index
+     * @return parent index
+     */
+    static int parent(int index) {
+        return (index - 1) / 2;
     }
 
-    // to get index of left child of node at index i
-    static int left(int i) {
-        return (2 * i + 1);
+    /**
+     * @param index
+     * @return left son index
+     */
+    static int left(int index) {
+        return (2 * index + 1);
     }
 
-    // to get index of right child of node at index i
-    static int right(int i) {
-        return (2 * i + 2);
+    /**
+     * @param index
+     * @return right son index
+     */
+    static int right(int index) {
+        return (2 * index + 2);
     }
 }
--- src/TransactionLib/src/main/java/PrimitivePriorityQueue.java
+++ src/TransactionLib/src/main/java/PrimitivePriorityQueue.java
@@ -2,19 +2,35 @@ package TransactionLib.src.main.java;
 
 import java.util.ArrayList;
 
-
+/**
+ * This is the basic implementation of the Priority Queue it supports the conventional priority queue API
+ * FOR COMPLEXITY CALCULATION THE PRIORITY QUEUE SIZE IS N AND IGNORED NUMBER IF IGNORED NODES IS L
+ */
 public class PrimitivePriorityQueue {
     protected ArrayList<PQObject> _heapContainer;
     protected int _ignoredCounter = 0;
 
+    /**
+     * simple constructor
+     */
     public PrimitivePriorityQueue() {
         this._heapContainer = new ArrayList<PQObject>();
     }
 
+    /**
+     * initialize size constructor
+     */
     public PrimitivePriorityQueue(int minCapacity) {
         this._heapContainer = new ArrayList<PQObject>(minCapacity);
     }
 
+    /**
+     * swapping between 2 location in the container
+     *
+     * @param i first index
+     * @param j second index
+     * @Complexity O(1)
+     */
     private void swap(int i, int j) {
         PQObject iObject = this._heapContainer.get(i);
         PQObject jObject = this._heapContainer.get(j);
@@ -27,6 +43,12 @@ public class PrimitivePriorityQueue {
 
     }
 
+    /**
+     * fixing the heap stating given index
+     *
+     * @param index index to start with fixing
+     * @Complexity O(log N)
+     */
     private void minHeapify(int i) {
         int heapSize = this._heapContainer.size();
         int l = PQObject.left(i);
@@ -45,7 +67,13 @@ public class PrimitivePriorityQueue {
         }
     }
 
-
+    /**
+     * dequeue from the priority queue it's root, the minimum priority node
+     *
+     * @return a reference of the dequeued node
+     * @throws TXLibExceptions.PQueueIsEmptyException
+     * @Complexity O(log N)
+     */
     PQObject singleDequeue() throws TXLibExceptions.PQueueIsEmptyException {
         int heapSize = this._heapContainer.size();
         if (heapSize <= 0) {
@@ -64,6 +92,13 @@ public class PrimitivePriorityQueue {
         return root;
     }
 
+    /**
+     * dequeue from the priority queue  the minimum priority node that not ignored
+     *
+     * @return a reference of the dequeued node
+     * @throws TXLibExceptions.PQueueIsEmptyException
+     * @Complexity O(L * log N)
+     */
     public PQObject dequeue() throws TXLibExceptions.PQueueIsEmptyException {
         PQObject root;
         do {
@@ -73,6 +108,13 @@ public class PrimitivePriorityQueue {
         return root;
     }
 
+    /**
+     * getter of the root of the priority queue ignored is not relevant here
+     *
+     * @return new node contains the head
+     * @throws TXLibExceptions.PQueueIsEmptyException
+     * @Complexity O(1)
+     */
     public PQObject top() throws TXLibExceptions.PQueueIsEmptyException {
         if (this._heapContainer.isEmpty()) {
             TXLibExceptions excep = new TXLibExceptions();
@@ -82,7 +124,13 @@ public class PrimitivePriorityQueue {
         return root;
     }
 
-
+    /**
+     * getter the minimum priority node that not ignored
+     *
+     * @return new node contains the head
+     * @throws TXLibExceptions.PQueueIsEmptyException
+     * @Complexity O(L * log N)
+     */
     public PQObject topWithClearIgnored() throws TXLibExceptions.PQueueIsEmptyException {
         try {
             while (this._heapContainer.get(0).getIsIgnored()) {
@@ -96,7 +144,13 @@ public class PrimitivePriorityQueue {
         return this._heapContainer.get(0);
     }
 
-
+    /**
+     * enqueue new node to the priority queue
+     *
+     * @param node2enqueue the new node to be enqueued
+     * @return a reference of the new node
+     * @Complexity amortized O(log N)
+     */
     protected final PQObject enqueue(PQObject node2enqueue) {
         assert !node2enqueue.getIsIgnored();
         // First insert the new key at the end
@@ -112,13 +166,26 @@ public class PrimitivePriorityQueue {
         return node2enqueue;
     }
 
-
+    /**
+     * enqueue new node with given priority and value to the priority queue
+     *
+     * @param priority priority of the new node
+     * @param value    value of the new node
+     * @return a reference of the new node
+     * @Complexity amortized O(log N)
+     */
     public final PQObject enqueue(Comparable priority, Object value) {
         PQObject newNode = new PQObject(priority, value);
         return this.enqueue(newNode);
     }
 
-
+    /**
+     * dequeue from the priority queue it's root, the minimum priority node
+     *
+     * @return a reference of the dequeued node
+     * @throws TXLibExceptions.PQueueIsEmptyException
+     * @Complexity O(log N)
+     */
     public void decreasePriority(final PQObject nodeToModify, Comparable newPriority) {
         assert nodeToModify != null;
         if (!this.containsNode(nodeToModify) || nodeToModify.compareTo(newPriority) <= 0) {
@@ -132,16 +199,35 @@ public class PrimitivePriorityQueue {
         }
     }
 
+    /**
+     * predicate the check whether the priority queue is empty
+     *
+     * @return true iff the priority queue is empty
+     * @Complexity O(1)
+     */
     public boolean isEmpty() {
         return this.size() == 0;
     }
 
+    /**
+     * getter of the priority queue size discount ignoring nodes
+     *
+     * @return the actual size of the priority queue
+     * @Complexity O(1)
+     */
     public int size() {
         int size = this._heapContainer.size() - this._ignoredCounter;
         assert size >= 0;
         return size;
     }
 
+    /**
+     * predicate the check whether a specific node in the priority queue
+     *
+     * @param node node to be search for
+     * @return true iff the given node is in the priority queue
+     * @Complexity O(1)
+     */
     public boolean containsNode(PQObject node) {
         int nodeIndex = node.getIndex();
         if (nodeIndex < 0 || nodeIndex >= this._heapContainer.size()) {
